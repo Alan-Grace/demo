01d7a48c0c6af5eb8711b7cdeffade44625d7538
sunjie <sunjie@lihetech.com>
2019-12-22 18:42:34 +0800

    7

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,15 +1,15 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
 
     @GetMapping("/test")
-    public Integer test() {
+    public String test() {
 
-        return 0;
+        return "Test";
     }
 
 }
